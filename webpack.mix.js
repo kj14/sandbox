d21a5eff5cff611fa354298d9990168a5682e07d
Koji Tanemura <tnmrkj+github@gmail.com>
2022-06-09 10:50:37 +0900

    typescriptの導入

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,13 +11,23 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+// mix.js('resources/js/app.js', 'public/js')
+mix.js("resources/js/app.ts", "public/js")
     .vue()
-    .postCss('resources/css/app.css', 'public/css', [require('tailwindcss'), require('autoprefixer')])
+    .postCss("resources/css/app.css", "public/css", [
+        require("tailwindcss"),
+        require("autoprefixer"),
+    ])
     .alias({
-        '@': 'resources/js',
+        "@": "resources/js",
     });
 
 if (mix.inProduction()) {
     mix.version();
 }
+
+mix.webpackConfig({
+    stats: {
+        children: true,
+    },
+});
